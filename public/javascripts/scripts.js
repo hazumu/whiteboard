e291c5ab770441d79f7aa7ec24ae529b2cc584b3
ikumi.toshihiko <ikumi.toshihiko@dena.jp>
2013-02-15 20:26:24 +0900

    modified script.js

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -44,6 +44,7 @@
 		canvasId : 'canvas',
 		init : function() {
 			canvas.element = document.getElementById(canvas.canvasId);
+			canvas.element.width = window.innerWidth;
 			canvas.ctx = canvas.element.getContext("2d");
 			canvas.isDrow = false;
 		},
