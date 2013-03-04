306d891fb82212dbb20c44d1555e9adad2a02b1b
Hazumu Jo <jo.hazumu@dena.jp>
2013-03-04 20:02:31 +0900

    add room feature

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -64,6 +64,9 @@
 			canvas.element.width = window.innerWidth;
 			canvas.ctx = canvas.element.getContext("2d");
 			canvas.isDrow = false;
+			if (window.bitmapData !== '') {
+				this.addImage();
+			}
 		},
 		// draw : function(type, data) {
 		// 	var ctx = canvas.ctx,
@@ -98,11 +101,24 @@
 			ctx.lineTo(tox, toy);
 			ctx.stroke();
 		},
+		addImage: function() {
+			var ctx = canvas.ctx;
+			var image = new Image();
+			image.onload = $.proxy(function() {
+				console.log(window.bitmapData);
+				console.log(ctx);
+				ctx.drawImage(image, 0, 0);
+			}, this);
+			image.src = window.bitmapData;
+		},
 		getPosX :function(data) {
 			return data.x || data.offsetX || data.changedTouches[0].clientX - data.changedTouches[0].target.offsetLeft;
 		},
 		getPosY :function(data) {
 			return data.y || data.offsetY || data.changedTouches[0].clientY - data.changedTouches[0].target.offsetTop;
+		},
+		getDataUrl: function() {
+			return canvas.element.toDataURL('image/png');
 		}
 	};
 
@@ -124,7 +140,8 @@
 
 			canvas.init();
 			canvas.element.addEventListener(EVT.start, app, false);
-
+			// save
+			$('#saveBtn').on('click', $.proxy(this.save, this));
 		},
 		handleEvent : function(e) {
 			e.preventDefault();
@@ -213,6 +230,23 @@
 		onNewConnected : function(e, data) {
 			console.log(data);
 			app.connectIdElm.append('<span>'+data.socketid+'</span>,');
+			canvas.draw(data.action, data);
+		},
+		save: function(e) {
+			console.log('click save button');
+			var url = '/room_save';
+
+			var data = canvas.getDataUrl(),
+					id = $(e.target).data('id');
+
+			$.ajax({
+				type: 'POST',
+				url: url,
+				data: {
+					id: id,
+					data: data
+				}
+			});
 		}
 	};
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
-
 /*
  * GET home page.
  */
-
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+	// model
+	var RoomProvider = require('../models/roomprovider').RoomProvider;
+
+	var list = RoomProvider.findAll(function(req, post){
+		res.render('index', {
+			title: 'ホワイトボードくん',
+			lists: post 
+		});
+	});
 };
